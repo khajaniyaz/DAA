14851fad6f5cd31c14aaeea5b6ccd3a4fe4e4643
khajaniyaz <khajaniyaz666@gmail.com>
2024-11-21 19:37:10 +0530

    Update two integers of array.py

--- Day-1/two integers of array.py
+++ Day-1/two integers of array.py
@@ -1,5 +1,4 @@
-'''You are given two integer arrays nums1 and nums2 of sizes n and m, respectively. Calculate the following values: answer1 : the number of indices i such that nums1[i] 
-exists in nums2. answer2 : the number of indices i such that nums2[i] exists in nums1 Return [answer1,answer2]'''
+
 def checker(num1, num2):
     ans=[0,0]
     for i in num1:
@@ -12,4 +11,4 @@ def checker(num1, num2):
 num1=[1,2,2,3,4]
 num2=[2,3,4,5,6]
 print(checker(num1,num2))
-#Output: answer1: 4 answer2: 3
+
